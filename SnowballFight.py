7c92ab6e99ff98329036c15a2c19b972301bf1ce
kyleyeh <kyle.yeh@d11.org>
2025-11-25 14:48:32 -0700

    Update SnowballFight.py

--- SnowballFight.py
+++ SnowballFight.py
@@ -6,14 +6,112 @@
     Python: 
 '''
 
-def main():
-    # the main runner of the game
-	  # welcome the player, gather names, and run the snowball fight!
-    print("Welcome to Snowball Mania!")
+import random
+import time
 
-def hitResult(hitNum):
-    # based on the number that is passed in, return True or False 
-    # indicating if this was a hit or a miss
-    return false
 
-main()
+def printIntro():
+    '''
+    ' Param: none
+    ' 
+    ' Print a welcome message to the user
+    ' 
+    ' Return: none
+    '''
+    print("❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️")
+    print("❄️  Welcome to Snowball Mania!❄️")
+    print("❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️")
+
+
+def getNames():
+    '''
+    ' Param: none
+    ' 
+    ' Create a list to hold player names.
+    ' Ask the user for their name.  Store it in a variable and add it to the player list.
+    ' Print instructions for the user to add more players and to type "DONE" when finished.
+    ' Read in the first additional player name.
+    ' While the user hasn't typed "DONE", add the new name to the player list and prompt for the next name
+    ' After the user is finished entering names, print a "Time to play!" message
+    '
+    ' Return: the list of player names
+    ' 
+    '''
+
+
+def getThrower(players):
+    '''
+    ' Param: players (list of player names)
+    '
+    ' Return a randomly chosen player name to be the next thrower.
+    '
+    ' Return: player name
+    '''
+
+    
+def getVictim(players, t):
+    '''
+    ' Param: players (list of player names), t (the thrower for this round)
+    ' 
+    ' Select a random player to be the next victim.  
+    ' While the victim is the same player as the thrower, randomly select a new victim from the list.
+    ' Return the victim's name.
+    '
+    ' Return: victim's name
+    '''
+
+
+def getHitResult():
+    '''
+    ' Param: none
+    ' 
+    ' Generate a random number between 1 and 10
+    ' If the number is greater than ___, return True
+    ' Else, return False
+    '
+    ' Return: Boolean representing whether or not the snowball hit
+    '''
+    
+
+def playSnowballFight(players):
+    '''
+    ' Param: players (a list of players still in the game)
+    '
+    ' While there are still multiple players in the game...
+    '   - Get the next thrower
+    '   - Get the next victim
+    '   - Get the next hit result
+    '   - If a hit occurred, flip a coin to see if it is a knockout or not.
+    '     - If knockout, print a knockout message and remove the victim from the list
+    '     - Else, print a hit message but do not remove victim
+    '   - Else, print a miss message
+    '   - time.sleep(3) - delay execution for three seconds
+    ' 
+    ' Return: none
+    '''
+
+    
+def printOutro(winner):
+    '''
+    ' Param: name of the winner
+    ' 
+    ' Print a congratulatory message naming the winner
+    '
+    ' Return: none
+    '''
+    print("❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️")
+    print("All hail " + winner + ", the Ultimate Student/Snowball Wizard!")
+    print("❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️")
+
+
+def runProgram():
+    '''
+    ' Param: none
+    ' 
+    ' Call the method that will print the intro messages
+    ' Call the method that will return a list of player names.  Save the list in a variable.
+    ' Call the method that will simulate the snowball fight
+    ' Call the method that will print the outro messagees
+    '
+    ' Return: none
+    '''
